024ae4566bd412874dfdb75caf456df55289216f
eeehaihai <163230973+eeehaihai@users.noreply.github.com>
2025-04-01 12:01:28 +0800

    Add files via upload

---
+++ example1.cpp
@@ -0,0 +1,183 @@
+// 同化棋（Ataxx）简易交互样例程序
+// 随机策略
+// 作者：zhouhy zys
+// 游戏信息：http://www.botzone.org/games#Ataxx
+
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int currBotColor; // 我所执子颜色（1为黑，-1为白，棋盘状态亦同）
+int gridInfo[7][7] = { 0 }; // 先x后y，记录棋盘状态
+int blackPieceCount = 2, whitePieceCount = 2;
+int startX, startY, resultX, resultY;
+static int delta[24][2] = { { 1,1 },{ 0,1 },{ -1,1 },{ -1,0 },
+{ -1,-1 },{ 0,-1 },{ 1,-1 },{ 1,0 },
+{ 2,0 },{ 2,1 },{ 2,2 },{ 1,2 },
+{ 0,2 },{ -1,2 },{ -2,2 },{ -2,1 },
+{ -2,0 },{ -2,-1 },{ -2,-2 },{ -1,-2 },
+{ 0,-2 },{ 1,-2 },{ 2,-2 },{ 2,-1 } };
+
+// 判断是否在地图内
+inline bool inMap(int x, int y)
+{
+	if (x < 0 || x > 6 || y < 0 || y > 6)
+		return false;
+	return true;
+}
+
+// 向Direction方向改动坐标，并返回是否越界
+inline bool MoveStep(int &x, int &y, int Direction)
+{
+	x = x + delta[Direction][0];
+	y = y + delta[Direction][1];
+	return inMap(x, y);
+}
+
+// 在坐标处落子，检查是否合法或模拟落子
+bool ProcStep(int x0, int y0, int x1, int y1, int color)
+{
+	if (color == 0)
+		return false;
+	if (x1 == -1) // 无路可走，跳过此回合
+		return true;
+	if (!inMap(x0, y0) || !inMap(x1, y1)) // 超出边界
+		return false;
+	if (gridInfo[x0][y0] != color)
+		return false;
+	int dx, dy, x, y, currCount = 0, dir;
+	int effectivePoints[8][2];
+	dx = abs((x0 - x1)), dy = abs((y0 - y1));
+	if ((dx == 0 && dy == 0) || dx > 2 || dy > 2) // 保证不会移动到原来位置，而且移动始终在5×5区域内
+		return false;
+	if (gridInfo[x1][y1] != 0) // 保证移动到的位置为空
+		return false;
+	if (dx == 2 || dy == 2) // 如果走的是5×5的外围，则不是复制粘贴
+		gridInfo[x0][y0] = 0;
+	else
+	{
+		if (color == 1)
+			blackPieceCount++;
+		else
+			whitePieceCount++;
+	}
+
+	gridInfo[x1][y1] = color;
+	for (dir = 0; dir < 8; dir++) // 影响邻近8个位置
+	{
+		x = x1 + delta[dir][0];
+		y = y1 + delta[dir][1];
+		if (!inMap(x, y))
+			continue;
+		if (gridInfo[x][y] == -color)
+		{
+			effectivePoints[currCount][0] = x;
+			effectivePoints[currCount][1] = y;
+			currCount++;
+			gridInfo[x][y] = color;
+		}
+	}
+	if (currCount != 0)
+	{
+		if (color == 1)
+		{
+			blackPieceCount += currCount;
+			whitePieceCount -= currCount;
+		}
+		else
+		{
+			whitePieceCount += currCount;
+			blackPieceCount -= currCount;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	istream::sync_with_stdio(false);
+
+	int x0, y0, x1, y1;
+
+	// 初始化棋盘
+	gridInfo[0][0] = gridInfo[6][6] = 1;  //|黑|白|
+	gridInfo[6][0] = gridInfo[0][6] = -1; //|白|黑|
+
+										  // 分析自己收到的输入和自己过往的输出，并恢复状态
+	int turnID;
+	currBotColor = -1; // 第一回合收到坐标是-1, -1，说明我是黑方
+	cin >> turnID;
+	for (int i = 0; i < turnID - 1; i++)
+	{
+		// 根据这些输入输出逐渐恢复状态到当前回合
+		cin >> x0 >> y0 >> x1 >> y1;
+		if (x1 >= 0)
+			ProcStep(x0, y0, x1, y1, -currBotColor); // 模拟对方落子
+		else
+			currBotColor = 1;
+		cin >> x0 >> y0 >> x1 >> y1;
+		if (x1 >= 0)
+			ProcStep(x0, y0, x1, y1, currBotColor); // 模拟己方落子
+	}
+
+	// 看看自己本回合输入
+	cin >> x0 >> y0 >> x1 >> y1;
+	if (x1 >= 0)
+		ProcStep(x0, y0, x1, y1, -currBotColor); // 模拟对方落子
+	else
+		currBotColor = 1;
+
+	// 找出合法落子点
+	int beginPos[1000][2], possiblePos[1000][2], posCount = 0, choice, dir;
+
+	for (y0 = 0; y0 < 7; y0++)
+		for (x0 = 0; x0 < 7; x0++)
+		{
+			if (gridInfo[x0][y0] != currBotColor)
+				continue;
+			for (dir = 0; dir < 24; dir++)
+			{
+				x1 = x0 + delta[dir][0];
+				y1 = y0 + delta[dir][1];
+				if (!inMap(x1, y1))
+					continue;
+				if (gridInfo[x1][y1] != 0)
+					continue;
+				beginPos[posCount][0] = x0;
+				beginPos[posCount][1] = y0;
+				possiblePos[posCount][0] = x1;
+				possiblePos[posCount][1] = y1;
+				posCount++;
+			}
+		}
+
+	// 做出决策（你只需修改以下部分）
+	int startX, startY, resultX, resultY;
+	if (posCount > 0)
+	{
+		srand(time(0));
+		choice = rand() % posCount;
+		startX = beginPos[choice][0];
+		startY = beginPos[choice][1];
+		resultX = possiblePos[choice][0];
+		resultY = possiblePos[choice][1];
+	}
+	else
+	{
+		startX = -1;
+		startY = -1;
+		resultX = -1;
+		resultY = -1;
+	}
+
+
+	
+	// 决策结束，输出结果（你只需修改以上部分）
+
+	cout << startX << " " << startY << " " << resultX << " " << resultY;
+	return 0;
+}
